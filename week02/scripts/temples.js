567357f6b90ce7145786ccaa3e69253e61fa7195
BenjaminS-BYU <bstrong2@byupathway.edu>
2025-11-04 19:18:06 -0700

    finished css

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,3 +1,5 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("copyright").innerHTML = `&copy; ${currentYear} | Benjamin Strong | Canada`;
 document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+

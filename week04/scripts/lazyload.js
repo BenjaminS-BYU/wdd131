2bd2b5c3a9e0b70cad00ca8ea9636c1a48bcab0e
BenjaminS-BYU <bstrong2@byupathway.edu>
2025-11-19 07:50:51 -0700

    Lazy loading

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,3 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("copyright").innerHTML = `&copy; ${currentYear} | Benjamin Strong | Canada`;
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
